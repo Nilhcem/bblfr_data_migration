35a84f9ca2c0f0f4c7f3662bd6179dee3a1ae809
Gautier Nilhcem <nilhcem@gmail.com>
2016-08-19 01:10:59 +0200

    Simplify DataProvider

--- src/main/kotlin/com/nilhcem/bblfr/migration/data/DataProvider.kt
+++ src/main/kotlin/com/nilhcem/bblfr/migration/data/DataProvider.kt
@@ -9,19 +9,17 @@ import java.io.File
 
 class DataProvider {
 
-    companion object {
-        val MOSHI = Moshi.Builder().build()
-    }
-
     val inputData: InputData
     val sinceMap: Map<String, String>
+
+    private val moshi = Moshi.Builder().build()
     private val outputFile: File
 
     constructor(args: Array<String>) {
         if (args.size != 3) throw IllegalArgumentException("Invalid args number.%nHave you run the app through the run.sh file?")
 
         val inputJson = File(args[0]).readText().replaceFirst(Regex("[^{]*"), "")
-        inputData = MOSHI.adapter(InputData::class.java).fromJson(inputJson)
+        inputData = moshi.adapter(InputData::class.java).fromJson(inputJson)
 
         sinceMap = File(args[1]).readLines().map {
             val spaceIdx = it.indexOf(" ")
@@ -40,7 +38,7 @@ class DataProvider {
             val buffer = Buffer()
             val jsonWriter = JsonWriter.of(buffer)
             jsonWriter.setIndent("\t")
-            MOSHI.adapter(OutputData::class.java).toJson(jsonWriter, output)
+            moshi.adapter(OutputData::class.java).toJson(jsonWriter, output)
             out.println(buffer.readUtf8())
         }
     }
